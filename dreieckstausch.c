619d584e68304a06f75b768287cc98744d08f468
cocherovski <cocher@gmx.de>
2016-09-26 10:03:50 +0200

    Update dreieckstausch.c

--- dreieckstausch.c
+++ dreieckstausch.c
@@ -1,5 +1,27 @@
 #include <iostream>
 using namespace std;
 
+void tausch(int &a, int &b);
+void tausch2(int *a, int *b);
+
 void main() {
-  
+	int a = 5;
+	int b = 1;
+	int *p1=&a, *p2=&b;
+	tausch(a, b);
+	cout << a << '\t' << b << '\n';
+	tausch2(p1, p2);
+	cout << a << '\t' << b << '\n';
+}
+
+void tausch(int &a, int &b) {
+	int c = a;
+	a = b;
+	b = c;
+}
+
+void tausch2(int *a, int *b) { // kann nicht gehen
+	int *c = a;
+	a = b;
+	b = c;
+}
